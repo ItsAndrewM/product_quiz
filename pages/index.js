11f94e9f835992435d6ae3282816e95da52a9f4f
Andrew McMillan <mcmillan.r.andrew@gmail.com>
2023-11-17 14:13:36 -0800

    added title screen

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,31 @@
 import Layout from "@/components/ui/layout/layout";
 import Link from "next/link";
+import questionStyles from "@/styles/question.module.css";
+import confirmStyles from "@/styles/confirm.module.css";
 
 //https://www.firstlite.com/pages/firstlite-kit-finder.html?lang=en_CA
 
 export default function Home() {
   return (
     <Layout>
-      <div>
-        <Link href={"/quiz/purpose"}>Start Quiz</Link>
+      <div className={questionStyles.wrapper}>
+        <div className={questionStyles.titleWrapper}>
+          <h1>Takacat Boat Model Finder</h1>
+          <p>
+            Takacat boats embody the spirit of adventure and deliver an
+            unparalleled on-water experience. With their exceptional
+            versatility, stability, comfort, durability, and user-friendly
+            design, Takacat day boats are the perfect choice for water
+            enthusiasts seeking excitement and unforgettable moments. Find
+            what&apos;s right for you.
+          </p>
+        </div>
+        <Link
+          href={"/quiz/purpose"}
+          className={`${confirmStyles.button} ${questionStyles.submit}`}
+        >
+          Start Quiz
+        </Link>
       </div>
     </Layout>
   );
