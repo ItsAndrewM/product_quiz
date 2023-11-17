beaf868f94502927b24a6e2a1bd0102c558d7acc
Andrew McMillan <mcmillan.r.andrew@gmail.com>
2023-11-17 14:59:56 -0800

    added progress bar

--- components/ui/navigation/breadcrumbs/breadcrumbs.jsx
+++ components/ui/navigation/breadcrumbs/breadcrumbs.jsx
@@ -6,11 +6,22 @@ import { useEffect, useState } from "react";
 const Breadcrumbs = () => {
   const router = useRouter();
   const [urlQuery, setUrlQuery] = useState(router.query);
+  const [progress, setProgress] = useState(0);
   useEffect(() => {
     const query = router.query;
     delete query.question;
     setUrlQuery(query);
-  }, [router.query]);
+    const index = breadcrumbs.findIndex((index) => {
+      console.log(index);
+      return router.asPath.split("?")[0].includes(index.slug);
+    });
+    if (router.asPath.split("?")[0].includes("/confirm")) {
+      setProgress(100);
+    } else {
+      setProgress((index + 1) * 12);
+    }
+  }, [router.query, router.asPath]);
+
   return (
     <div className={styles.wrapper}>
       <ul className={styles.list}>
@@ -40,6 +51,11 @@ const Breadcrumbs = () => {
           </Link>
         </li>
       </ul>
+      <progress
+        className={styles.progress}
+        value={progress}
+        max={100}
+      ></progress>
     </div>
   );
 };
